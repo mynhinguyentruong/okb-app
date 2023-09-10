67047c8dd5b49d35b8f15ac0c3b1934851c626f5
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-09-10 14:49:14 -0400

    add different method to get price from uniswap pool

--- utils/get_usdc_eth_price.ts
+++ utils/get_usdc_eth_price.ts
@@ -1,5 +1,6 @@
 import { ethers } from "ethers"
 import { aggregatorV3InterfaceABI } from "@/utils/abi"
+import { uniswapV3PoolABI } from "@/utils/uniswapv3pool_abi"
 
 export async function getUSDCETHPrice(): Promise<number> {
     const provider = new ethers.AlchemyProvider("mainnet",process.env.ALCHEMY_KEY as string) 
@@ -13,6 +14,28 @@ export async function getUSDCETHPrice(): Promise<number> {
 
     let decimals = await priceFeed.decimals();
 
-    return Number(roundData.answer.toString()) / Math.pow(10, Number(decimals));
+    return (Number(roundData.answer.toString()) / Math.pow(10, Number(decimals)));
 }
 
+export async function getPriceFromSQRTPriceX96(): Promise<number> {
+  const provider = new ethers.AlchemyProvider("mainnet",process.env.ALCHEMY_KEY as string) 
+
+  // UniswapV3Pool Address of USDC/ETH
+  // https://etherscan.io/address/0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8#code
+  const addr = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"
+
+  const contract = new ethers.Contract(addr, uniswapV3PoolABI, provider);
+
+  const sqrtPriceX96 = (await contract.slot0()).sqrtPriceX96
+
+  const Q96 = BigInt(Math.pow(2, 96))
+
+  const P = (sqrtPriceX96/Q96)*(sqrtPriceX96/Q96)
+  const ethPrice = (Math.pow(10, 12)) / Number(P) 
+
+  console.log({ ethPrice })
+  console.log({ ethPrice })
+  console.log({ ethPrice })
+
+  return ethPrice
+}
